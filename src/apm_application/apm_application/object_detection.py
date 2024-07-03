30f8e0fa93f9425a659d5eae5bc4c90e8156ac44
robo <robo2020@gmail.com>
2024-07-03 13:10:36 +0000

    fix projection and add axis

--- src/apm_application/apm_application/object_detection.py
+++ src/apm_application/apm_application/object_detection.py
@@ -6,8 +6,11 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 import math
+from geometry_msgs.msg import TransformStamped
+from tf2_ros import TransformBroadcaster
 
 CAMERA_TOPIC = "/gimbal_camera/image_raw"
+AXIS = np.float32([[1.2,0,0], [0,1.2,0], [0,0,-1.2]]).reshape(-1,3)
 
 class ObjectTracker(Node):
     def __init__(self):
@@ -16,9 +19,36 @@ class ObjectTracker(Node):
         self.publisher_ = self.create_publisher(String, 'object_coordinates', 10)
         self.image_publisher_ = self.create_publisher(Image, 'object_tracking/image_raw', 10)
         self.subscription = self.create_subscription(Image, CAMERA_TOPIC, self.image_callback, 10)
-        # self.back_sub = cv2.createBackgroundSubtractorMOG2(history=700, varThreshold=25, detectShadows=True)
-        # self.kernel = np.ones((20,20),np.uint8)
+        self.tf_broadcaster = TransformBroadcaster(self)
         self.bridge = CvBridge()
+
+    # def send_tf(self, tvec, rvec):
+    #     t = TransformStamped()
+
+    #     # Read message content and assign it to
+    #     # corresponding tf variables
+    #     t.header.stamp = self.get_clock().now().to_msg()
+    #     t.header.frame_id = 'world'
+    #     t.child_frame_id = "tracker"
+
+    #     # Turtle only exists in 2D, thus we get x and y translation
+    #     # coordinates from the message and set the z coordinate to 0
+    #     t.transform.translation.x = msg.x
+    #     t.transform.translation.y = msg.y
+    #     t.transform.translation.z = 0.0
+
+    #     # For the same reason, turtle can only rotate around one axis
+    #     # and this why we set rotation in x and y to 0 and obtain
+    #     # rotation in z axis from the message
+    #     q = quaternion_from_euler(0, 0, msg.theta)
+    #     t.transform.rotation.x = q[0]
+    #     t.transform.rotation.y = q[1]
+    #     t.transform.rotation.z = q[2]
+    #     t.transform.rotation.w = q[3]
+
+    #     # Send the transformation
+    #     self.tf_broadcaster.sendTransform(t)
+
 
     def image_callback(self, msg):
         mask = 0
@@ -43,10 +73,10 @@ class ObjectTracker(Node):
         x, y, w, h = cv2.boundingRect(cnt)
 
         obj_p = np.array([
-                [x, y, 0],
-                [x + w, y, 0],
-                [x + w, y + h, 0],
-                [x, y + h, 0]
+                [0, 0, 0],
+                [1, 0, 0], 
+                [1, 1, 0], 
+                [0, 1, 0]
             ],dtype=np.float32)
         
         img_p = np.array([
@@ -61,22 +91,35 @@ class ObjectTracker(Node):
         # img_p[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1,2)
 
         print(x,y,w,h)
-        size = (640, 480)
+        size = (640, 512)
+        fl = 686
         camera_matrix = np.array(
-            [[630, 0, size[0]/2],
-             [0, 630, size[1]/2],
-             [0,0,1]], dtype=np.double
+            [[fl, 0, size[0]/2],
+             [0, fl, size[1]/2],
+             [0,0,1]], dtype=np.float32
         )
-        dist_coeffs = np.zeros((4,1)) # no lens distortion
-        _, rvec, tvec = cv2.solvePnP(obj_p, img_p, camera_matrix, dist_coeffs)
-        # Convert rotation vector to Euler angles
-        rmat, _ = cv2.Rodrigues(rvec)
-        pitch = math.atan2(rmat[2, 0], rmat[2, 2])
-        yaw = math.atan2(rmat[2, 1], rmat[2, 2])
 
-        # Calculate position
-        position = tvec[:2]  
-        print(position)
+        dist_coeffs = np.zeros((4,1)) # no lens distortion
+        # success, rotation_vector, translation_vector = cv2.solvePnPRansac(
+        #     obj_p, img_p, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_RANSAC
+        # )
+
+        # if success:
+        #     print("Rotation Vector:", rotation_vector)
+        #     print("Translation Vector:", translation_vector)
+
+        _, rvec, tvec = cv2.solvePnP(obj_p, img_p, camera_matrix, dist_coeffs)
+        # print(tvec)
+        # self.draw(frame,(int(x),int(y)),imgpts)
+        cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec, tvec, 1.2) 
+        # # Convert rotation vector to Euler angles
+        # rmat, _ = cv2.Rodrigues(rvec)
+        # pitch = math.atan2(rmat[2, 0], rmat[2, 2])
+        # yaw = math.atan2(rmat[2, 1], rmat[2, 2])
+
+        # # Calculate position
+        # position = tvec[:2]  
+        # print(position)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 3)
 
         x2 = x + int(w/2)
